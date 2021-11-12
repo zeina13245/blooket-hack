ffec60b95a7af41f38f414910eac507f621fb84f
glizzz_y <73669084+glixzzy@users.noreply.github.com>
2021-11-12 13:59:03 -0600

    change variable name

--- global/addTokens.js
+++ global/addTokens.js
@@ -6,10 +6,10 @@ async function getName(authToken) {
 };
 
 async function addCurrencies() {
-    const add_tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
     const myToken = localStorage.token.split('JWT ')[1];
 
-    if (add_tokens > 500) {
+    if (tokens > 500) {
         alert('You can add up to 500 tokens daily.')
     }
 
@@ -21,14 +21,14 @@ async function addCurrencies() {
             "authorization": localStorage.token
         },
         body: JSON.stringify({
-            addedTokens: add_tokens,
+            addedTokens: tokens,
             addedXp: 300,
             name: await getName(myToken)
         })
     });
 
     if (response.status == 200) {
-        alert(`${add_tokens} tokens and 300 XP added to your account!`);
+        alert(`${tokens} tokens and 300 XP added to your account!`);
     } else {
         alert('An error occured.');
     };
